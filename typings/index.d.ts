fc7e1a3fa89e75ad5d9b22875e3ce71f38ba7c84
Takeo Asai <spitson+github@gmail.com>
2016-09-03 07:01:41 +0900

    Add 'require()' available

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,3 +2,5 @@
 /// <reference path="globals/angular-ui-router/index.d.ts" />
 /// <reference path="globals/angular/index.d.ts" />
 /// <reference path="globals/jquery/index.d.ts" />
+
+declare function require(x: string): any;
