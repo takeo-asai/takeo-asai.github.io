74dce24e7453530c224f499c91e04126908490e6
Takeo Asai <spitson+github@gmail.com>
2016-09-02 16:33:40 +0900

    Add Component

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,28 @@ import 'angular-ui-bootstrap';
 import "bootstrap-loader";
 
 angular.module("starter", ["ui.router", "ui.bootstrap"])
+
+  .component('listComponent', {
+    bindings: {
+      message: '=',
+    },
+    controller: class {
+      public message: string;
+      constructor() {
+        "ngInject";
+        this.message = "test";
+      }
+    },
+    template: `<div>Hello {{ $ctrl.message }}</div>`,
+  })
+
   .config(function ($stateProvider: ng.ui.IStateProvider, $urlRouterProvider: ng.ui.IUrlRouterProvider) {
     "ngInject";
 
     $stateProvider
       .state("app", {
         url: "/",
-        templateUrl: "src/index.html",
-//        controller: "DashCtrl",
-        controllerAs: "$ctrl",
+        templateUrl: "src/index.html"
       })
 
       ;
