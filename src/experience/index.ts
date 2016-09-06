759a668fe258e44d2b798e60ae04f7fc703f51f7
Takeo Asai <spitson+github@gmail.com>
2016-09-06 09:07:11 +0900

    Rename val

--- src/experience/index.ts
+++ src/experience/index.ts
@@ -4,16 +4,9 @@ import {Component, Input} from "@angular/core";
   selector: "experience-component",
   template: require('./template.html'),
   styles: [require("./style.css")],
-  
 })
 export default class ExperienceComponent {
-  @Input() name: string;
+  @Input() companyName: string;
   @Input() description: string;
-  constructor() {
-//    this.name = "hello world!";
-//    this.description = "description";
-  }
-  public ngOnInit() {
-  //  console.log("ngOnInit()", this.name, this.description);
-  }
+  constructor() { }
 }
