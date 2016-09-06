88da0833b3d270a430b6a20be6ceb9496aaf5bad
Takeo Asai <spitson+github@gmail.com>
2016-09-06 10:17:36 +0900

    Add serviceWorker defnition

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -3,3 +3,4 @@
 /// <reference path="globals/angular/index.d.ts" />
 /// <reference path="globals/es6-shim/index.d.ts" />
 /// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/service_worker_api/index.d.ts" />
